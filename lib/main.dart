f62425b42b1552fd34e4670be4ae428828604a97
Guilherme Pendezza de Sousa <guilherme.sousa@loft.com.br>
2020-12-14 06:52:29 -0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,16 @@
 import 'package:bytebank_persistence/screens/contacts_list.dart';
 import 'package:flutter/material.dart';
-import 'screens/contact_form.dart';
-import 'screens/dashboard.dart';
+import 'database/app_database.dart';
+import 'models/contact.dart';
 
-void main() => runApp(Bytebank());
+// 8.7
+
+void main() {
+  runApp(Bytebank());
+  save(Contact(1, 'Guilherme', 1234)).then((id) {
+    findAll().then((contacts) => debugPrint(contacts.toString()));
+  });
+}
 
 // ripple
 
--- lib/screens/contact_form.dart
+++ lib/screens/contact_form.dart
@@ -45,7 +45,7 @@ class ContactForm extends StatelessWidget {
                   onPressed: () {
                     final String name = _nameController.text;
                     final int accountNumber = int.tryParse(_accountNumberController.text);
-                    final Contact newContact = Contact(name, accountNumber);
+                    final Contact newContact = Contact(0, name, accountNumber);
                     Navigator.pop(context, newContact);
                   },
                   child: Text('Criar contato')
